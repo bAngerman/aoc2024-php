b3b472ebb233b00ed9031b048e5ee84a8b4919b1
Brendan Angerman <brendan.angerman@gmail.com>
2024-12-02 09:12:14 -0700

    cleanup

--- src/Day1/Day1.php
+++ src/Day1/Day1.php
@@ -13,20 +13,8 @@ class Day1 extends BaseClass
         $this->filePath = __DIR__ . '/input-example.txt';
     }
 
-    // public function parseInput() : void
-    // {
-    //     $pairs = [];
-
-    //     foreach ($this->data as $line) {
-    //         $numbers = explode("   ", $line);
-    //         $pairs[] = [(int) $numbers[0], (int) $numbers[1]];
-    //     }
-    // }
-
     public function run() : void
     {
-        // $this->parseInput();
-    
         $this->part1();
         $this->part2();
     }
